ad8bdfc7eb943b70e4df64afeae14e16ab460a87
matdasi <55964452+matdasi@users.noreply.github.com>
2019-10-31 09:43:39 +0900

    PushPrint.java modified on GitHub

--- HelloWorld/src/PushPrint.java
+++ HelloWorld/src/PushPrint.java
@@ -2,7 +2,8 @@
 public class PushPrint {
 
 	public static void main(String[] args) {
-System.out.println("Print Test");
+		System.out.println("Print Test");
+		System.out.println("GitGub Print Test");
 	}
 
 }
