6128713dfd0c99df13b9a504bf78438bb17d6ee6
ParkChangEun <78ckddms@gmail.com>
2019-10-31 09:44:40 +0900

    PushPrint.java modified on Eclipse

--- HelloWorld/src/PushPrint.java
+++ HelloWorld/src/PushPrint.java
@@ -2,8 +2,8 @@
 public class PushPrint {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("Print Test");
+		System.out.println("Print Test");
+		System.out.println("Eclipse Print Test");
 	}
 
 }
